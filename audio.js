e2a3203f4d3c8445495346a4c92be626a50a0618
cwyii <160295905+cwyii@users.noreply.github.com>
2024-02-21 21:23:52 -0600

    Update audio.js

--- audio.js
+++ audio.js
@@ -8,8 +8,64 @@ const audio = {
     },
     {
       src: '02-keys-to-my-life.mp3',
-      type: 'audio/ogg'
-    }
+      type: 'audio/mpeg'
+    },
+    {
+      src: '03-paid.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '04-talking.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '05-back-to-me.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '06-hoodrat.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '07-do-it.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '08-paperwork.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '09-burn.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '10-fuk-sumn.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '11-vultures.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '12-carnival.mp3',
+      type: 'audio/mpeg'
+    },
+    {
+      src: '13-beg-forgiveness.mp3',
+      type: 'audio/mpeg'
+    },
+     {
+      src: '14-good-(don't-die).mp3',
+      type: 'audio/mpeg'
+    },
+     {
+      src: '15-problematic.mp3',
+      type: 'audio/mpeg'
+    },
+     {
+      src: '16-king.mp3',
+      type: 'audio/mpeg'
+    },
   ]
 }
 const theDiv = document.querySelector("#audio");
