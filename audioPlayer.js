99c1ab978d706a2398ad32b4391dcc159b648028
cwyii <160295905+cwyii@users.noreply.github.com>
2024-02-22 13:16:18 -0600

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -10,8 +10,26 @@ function audioPlayer(){
                 currentSong = $(this).parent().index();
                 $(this).parent().addClass("current-song");
             });
-        
-        audio[0].addEventListener('ended',function(e){
+        var audio;
+var playlist;
+var tracks;
+var current;
+
+init();
+function init(){
+    current = 0;
+    audio = $('audio');
+    playlist = $('#playlist');
+    tracks = playlist.find('li a');
+    len = tracks.length - 1;
+
+    playlist.find('a').click(function(e){
+        e.preventDefault();
+        link = $(this);
+        current = link.parent().index();
+        run(link, audio[0]);
+    });
+    audio[0].addEventListener('ended',function(e){
 
     if(current == len){
         current = 0;
@@ -22,4 +40,13 @@ function audioPlayer(){
     }
     run($(link),audio[0]);
 });
+    });
+}
+function run(link, player){
+        player.src = link.attr('src');
+        par = link.parent();
+        par.addClass('active').siblings().removeClass('active');
+        audio[0].load();
+        audio[0].play();
+}
         }
