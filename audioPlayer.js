3dfa251b5b706a36812bd923cc7013026ddd8df2
cwyii <160295905+cwyii@users.noreply.github.com>
2024-02-21 23:01:11 -0600

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -20,23 +20,4 @@
                 $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
                 $("#audioPlayer")[0].play();
             });
-            
-let playlist = [
-    '01-stars.mp3',
-    '02-key-to-my-life.mp3',
-    '03-paid.mp3',
-    '04-talking.mp3',
-    '05-back-to-me.mp3',
-        '06-hoodrat.mp3',
-        '07-do-it.mp3',
-        '08-paperwork.mp3',
-        '09-burn.mp3',
-        '10-fuk0-sumn.mp3',
-        '11-vultures.mp3',
-        '12-carinval.mp3',
-        '13-beg-forgiveness.mp3',
-        '14-good-dont-die.mp3',
-        '15-problematic.mp3',
-        '16-king.mp3',
-];
         }
