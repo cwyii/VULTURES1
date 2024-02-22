512b8d7c4176cb1a4d97ade5a3cd1c9148a667f2
cwyii <160295905+cwyii@users.noreply.github.com>
2024-02-22 13:26:46 -0600

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -10,43 +10,13 @@ function audioPlayer(){
                 currentSong = $(this).parent().index();
                 $(this).parent().addClass("current-song");
             });
-        var audio;
-var playlist;
-var tracks;
-var current;
-
-init();
-function init(){
-    current = 0;
-    audio = $('audio');
-    playlist = $('#playlist');
-    tracks = playlist.find('li a');
-    len = tracks.length - 1;
-
-    playlist.find('a').click(function(e){
-        e.preventDefault();
-        link = $(this);
-        current = link.parent().index();
-        run(link, audio[0]);
-    });
-    audio[0].addEventListener('ended',function(e){
-
-    if(current == len){
-        current = 0;
-        link = playlist.find('a')[0];
-    }else{
-        link = playlist.find('a')[current];    
-        current++; // <-- Increment after the check
-    }
-    run($(link),audio[0]);
-});
-    });
-}
-function run(link, player){
-        player.src = link.attr('src');
-        par = link.parent();
-        par.addClass('active').siblings().removeClass('active');
-        audio[0].load();
-        audio[0].play();
-}
+        $("#audioPlayer")[0].addEventListener("ended", function(){
+               currentSong++;
+                if(currentSong == $("#playlist li a").length)
+                    currentSong = 1;
+                $("#playlist li").removeClass("current-song");
+                $("#playlist li:eq("+currentSong+")").addClass("current-song");
+                $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+                $("#audioPlayer")[0].play();
+            });
         }
